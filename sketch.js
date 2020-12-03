5308b999e24981d0a33177343d957ecec0d3654e
reshon12 <69683590+reshon12@users.noreply.github.com>
2020-12-03 19:41:26 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,24 +1,28 @@
-var ball;
+var ball,database;
+var position
 
 function setup(){
+    database=firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballposition=database.ref('ball/position');
+    ballposition.on("value",readPosition,showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
@@ -27,3 +31,15 @@ function changePosition(x,y){
     ball.x = ball.x + x;
     ball.y = ball.y + y;
 }
+function writePosition(x,y){
+     database.ref('ball/position').set({
+          'x': position.x + x , 'y': position.y + y }) }
+ function readPosition(data){ position = data.val();
+     console.log(position.x);
+     ball.x = position.x;
+     ball.y = position.y;
+     } 
+ function showError(){ 
+     console.log("Error in writing to the database"); 
+    }
+
